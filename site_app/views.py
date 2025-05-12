0c277b169494b9b94b53713c1d813d050e66e945
rigczTheSky <tmarszelewski@gmail.com>
2025-05-13 00:48:55 +0200

    start

---
+++ site_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "site_app/index.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+    return render(request, "site_app/detail.html", context)
